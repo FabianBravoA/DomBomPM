1a9f6367177615eb1f9115a23d41faf6972d25f2
Fabián Alexis Bravo Abarca <fabian.bravo.abarca@gmail.com>
2017-11-02 16:09:37 -0300

    Validación de número con alerta en nodo P

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ window.onload = function() {
     var textInput = document.getElementById("ingresa");
     //Evento para el input
     textInput.oninput = function() {
+        var inputDiv = document.getElementById("inputDiv");
+        var oldAlert = document.getElementById("alerta");
+        if (oldAlert) {
+            inputDiv.removeChild(oldAlert);
+        }
+
         if (isNaN(parseInt(textInput.value))) {
-            alert("No es un número");
+            var pAlert = document.createElement("p");
+            pAlert.id = "alerta";
+            pAlert.textContent = "No es un número y debería serlo";
+            pAlert.style.color = "red";
+            pAlert.style.fontStyle = "bold";
+
+            inputDiv.appendChild(pAlert);
         }
     }
 }
